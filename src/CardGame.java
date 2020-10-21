f5b7396cd9ddcc101efba23525487194c32c78ae
DGrad <53345463+Karna47@users.noreply.github.com>
2020-10-21 18:25:18 +0100

    '.'

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,25 +1,24 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class CardGame {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String fileName = "";
+        FileReader reader = null;
+
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Card Game Sim!");
         System.out.println("How many players should play the game?");
         int playerNumber = scanner.nextInt();
         System.out.println("What deck should they use?");
-        String deckName = scanner.nextLine();
         try{
-            File myFile = new File(deckName);
-            Scanner myReader = new Scanner(myFile);
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-        } catch (FileNotFoundException e) {
+            fileName = scanner.nextLine();
+            reader = new FileReader(fileName);
+            while (reader.)
+        } catch (IOException e){
             e.printStackTrace();
+            return;
         }
     }
 }
